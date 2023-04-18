ba1f301d9d7d31dbb0b4c7ce1117d64c9328a200
Kenny Cheng <kennycheng0206@gmail.com>
2023-04-17 20:03:28 -0400

    add scripts

---
+++ dot_zsh_scripts/functions.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# FUNCTIONS
+# =========
+
+
+# Get current Platform
+get-platform() {
+  case "$(uname -s)" in
+    Darwin)
+      echo 'apple'
+      ;;
+
+    Linux)
+      echo 'linux'
+      ;;
+
+    CYGWIN*|MINGW32*|MSYS*)
+      echo 'windows'
+      ;;
+
+    *)
+      echo 'unknown'
+      exit 1
+      ;;
+  esac
+}
+
+
+# Pretty Print JSON Curl Responses
+# Need to have jq installed
+jcurl() {
+  curl -s "$@" | jq .
+}
+
+# List all of current user's processes
+myps() { ps $@ -u $USER -o pid,%cpu,%mem,start,time,bsdtime,command ; }
+
+
+# Find CPU and Memory Hogs
+cpuhogs() { ps wwaxr -o pid,stat,%cpu,time,command | head -10 ;}
+memhogs() { ps wwaxm -o pid,stat,vsize,rss,time,command | head -10; }
